f2645c2bb8259eb9f98f27bfecf72a1d7d3883fc
LeThanhKiet <thanhkiet722000@gmail.com>
2023-01-12 13:58:46 +0700

    RemoveDuplicatesFromSortedList

---
+++ Java/Daily_83.java
@@ -0,0 +1,30 @@
+public class Daily_83 {
+     class ListNode{
+          int val;
+          ListNode next;
+          ListNode() {}
+          ListNode(int val) {
+               this.val = val; 
+          }
+          ListNode(int val, ListNode next) { 
+               this.val = val; this.next = next; 
+          }
+     }
+
+
+     public static ListNode deleteDuplicates(ListNode node) {
+        if(node == null){
+            return node;
+        }
+        ListNode head = node;
+        while(node.next != null){
+            if(node.val == node.next.val){
+                node.next = node.next .next;
+            }
+            else{
+                node = node.next;
+            }
+        }
+        return head;
+    }
+}

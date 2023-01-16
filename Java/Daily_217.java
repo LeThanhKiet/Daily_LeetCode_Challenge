6ac612297a3644c68f2ea62d9968f2fac6eaf240
LeThanhKiet <thanhkiet722000@gmail.com>
2023-01-16 16:53:07 +0700

    Contains Duplicate

---
+++ Java/Daily_217.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class Daily_217 {
+     public static boolean containsDuplicate(int[] nums){
+          Arrays.sort(nums);  
+          for(int i = 0; i < nums.length - 1; i++){
+               if(nums[i] == nums[i + 1]) return true;
+          }
+
+          return false;
+     }
+}

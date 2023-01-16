6596bcd51a78c7f7e1daa4bdbd059bdb6c78f9c9
LeThanhKiet <thanhkiet722000@gmail.com>
2023-01-16 21:31:30 +0700

    Contains Duplicate

--- Java/Daily_217.java
+++ Java/Daily_217.java
@@ -6,6 +6,7 @@ public class Daily_217 {
           for(int i = 0; i < nums.length - 1; i++){
                if(nums[i] == nums[i + 1]) return true;
           }
+          
 
           return false;
      }

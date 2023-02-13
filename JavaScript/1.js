86238f5baa55a356526f736c79f94798679b50e9
LeThanhKiet <thanhkiet722000@gmail.com>
2023-02-13 18:32:21 +0700

    Two Sum

--- JavaScript/1.js
+++ JavaScript/1.js
@@ -7,7 +7,9 @@
 let twoSum = (nums, target) => {
   for (let i = 0; i < nums.length - 1; i++) {
     for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] + nums[j] == target) return [i, j];
+      if (nums[i] + nums[j] == target) {
+        return [i, j];
+      }
     }
   }
 };

fbae81918a2b167f61ffc8cd2c53b3d709fb092d
LeThanhKiet <thanhkiet722000@gmail.com>
2023-04-12 18:25:36 +0700

    How Many Numbers Are Smaller Than the Current Number

--- Java/Daily_1365.java
+++ Java/Daily_1365.java
@@ -1,11 +1,13 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 public class Daily_1365 {
 
      public static void main(String[] args) {
           int[] nums = {8,1,2,2,3};
-          int[] arr = smallerNumbersThanCurrent(nums);
-          Arrays.stream(arr).forEach(System.out::println);
+          Arrays.stream(smallerNumbersThanCurrent1(nums)).forEach(System.out::println);
      }
+
      public static int[] smallerNumbersThanCurrent(int[] nums) {
           int[] arr = new int[nums.length];
           for (int i = 0; i < nums.length ; i++) {
@@ -19,6 +21,23 @@ public class Daily_1365 {
           }
 
           return arr;
+     }
+
+     public static int[] smallerNumbersThanCurrent1(int[] nums) {
+          Map<Integer, Integer> myMap = new HashMap<Integer, Integer>();
+          int[] copy = nums.clone();
+
+          Arrays.sort(copy);
+          for (int i = 0; i < nums.length; i++) {
+               myMap.putIfAbsent(copy[i], i);
+          }
+
+          for (int i = 0; i < nums.length; i++) {
+               copy[i] = myMap.get(nums[i]);
+          }
+
+          return copy;
+
      }
 
      

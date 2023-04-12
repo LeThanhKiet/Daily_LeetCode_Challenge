e046db00d852e8198ee4de8996d62acb0f1d66f9
LeThanhKiet <thanhkiet722000@gmail.com>
2023-04-12 18:12:35 +0700

    Two Sum

--- Java/Daily_1.java
+++ Java/Daily_1.java
@@ -1,13 +1,10 @@
+import java.util.Arrays;
 import java.util.HashMap;
 public class Daily_1 {
     public static void main(String[] args) {
         int nums[] = {2,7,11,15}, target = 9;
-        int res[] = twoSum(nums, target);
-        for(int i : res){
-            System.out.print(i + " ");
-        }
+        Arrays.stream(twoSum(nums, target)).forEach(System.out::println);
     }
-
     public static int[] twoSum(int[] nums, int target) {
         for(int i = 0; i < nums.length - 1; i++){
             for(int j = i + 1; j < nums.length; j++){
